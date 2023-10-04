fbed0800c9d1be4d13979d8ec5e8b531738858a1
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-04 20:47:44 +0200

    Add project files.

---
+++ 11.  Concatenation of strings/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim text1, text2, text3, text4 As String
+
+        text1 = "visual"
+        text2 = "Basic"
+        text3 = "2022"
+
+        text4 = text1 + " " + text2 + " " + text3
+        MsgBox(text4)
+
+
+
+    End Sub
+End Class
